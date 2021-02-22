ae402a6d4bff3cc5da79d2d6f25137d6fb16058e
Xalzer <47343622+Xalzer@users.noreply.github.com>
2021-02-21 22:45:43 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -88,21 +88,7 @@ function drawHouse() {
 
 }
 drawImage();
-//sun
-//var ball = {
-    //x: 100,
-    //y: 100,
-    //radius: 25,
-    //color: 'yellow',
-    //draw: function() {
-    //ctx.beginPath();
-    //ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2,             true);
-    //ctx.closePath();
-    //ctx.fillStyle = this.color;
-    //ctx.fill();
-//}
-//};
-//ball.draw();
+
 
 //clouds
   var clouds = {
